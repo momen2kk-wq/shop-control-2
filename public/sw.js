4affd906a43fef33c6d91edfef3ab9ac8753cc7c
momen2kk-wq <momen2kk@gmail.com>
2026-09-16 06:02:07 +0100

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+const CACHE="shop-control-prod-v2";
+self.addEventListener("install",event=>{event.waitUntil(self.skipWaiting())});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  const url=new URL(event.request.url);
+  if(url.origin===self.location.origin && url.pathname.startsWith("/api/")) return;
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});
